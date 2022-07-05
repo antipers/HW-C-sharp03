200d53ed81c27d65162ee62c4e559313003d332c
antipers <antipers@gmail.com>
2022-07-06 01:31:42 +0300

    trying complete task

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите пятизначное число- ");
+int Num;
+while (!int.TryParse(Console.ReadLine(), out Num))
+{
+    System.Console.WriteLine("Введен некорректный символ, введите пятизначное ЧИСЛО");
+}
+
+System.Console.WriteLine($"Число {Num} удовлетворяет критериям");
+
+while (Num > 99999 | Num < 10000)
+{
+    System.Console.WriteLine("Введено некорректное число, введите пятизначное");
+    int.TryParse(Console.ReadLine(), out Num);
+}
+
+int[] array = new int[5];
+for (int i = 1; i  <5; i++)
+{
+    int divNumb = 10000;
+    for (; divNumb > 0; divNumb /= 10)
+    {
+        array[i] = Num / divNumb;
+    }
+
+    System.Console.Write($"{array[i]}" + "  ");
+}
