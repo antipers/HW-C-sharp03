c10e1d4ba3689868ae14a2884a04eb1bfd7ed020
antipers <antipers@gmail.com>
2022-07-07 01:23:04 +0300

    completed

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("введите любое ЧИСЛО");
+int Num;
+while (!int.TryParse(Console.ReadLine(), out Num))
+{
+    System.Console.WriteLine("Введен некорректный символ, введите  ЧИСЛО");
+}
+
+System.Console.WriteLine($"Число {Num} удовлетворяет критериям");
+
+for (int i = 1; i < Num; i++)
+
+{
+
+    int result = i * i * i;
+    System.Console.Write($"{result} ,");
+
+}
+System.Console.WriteLine("\b \b");
