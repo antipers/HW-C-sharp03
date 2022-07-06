b41e0914195e417b41f54ccbab7a6ff2f50e9d8e
antipers <antipers@gmail.com>
2022-07-06 15:26:56 +0300

    completed

---
+++ task21/Program.cs
@@ -0,0 +1,17 @@
+﻿void printGipo3D(double aX, double aY, double aZ, double bX, double bY, double bZ)
+{
+    double dX = aX - bX;
+    double dY = aY- bY;
+    double dZ = aZ - bZ;
+    double rangeC = Math.Sqrt(dX * dX + dY * dY + dZ*dZ);
+    System.Console.WriteLine(rangeC);
+}
+
+System.Console.WriteLine("Введите координаты ");
+double aX = Convert.ToInt32(Console.ReadLine());
+double aY = Convert.ToInt32(Console.ReadLine());
+double aZ = Convert.ToInt32(Console.ReadLine());
+double bX = Convert.ToInt32(Console.ReadLine());
+double bY = Convert.ToInt32(Console.ReadLine());
+double bZ = Convert.ToInt32(Console.ReadLine());
+printGipo3D(aX, aY, aZ, bX, bY, bZ);
